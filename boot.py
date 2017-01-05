60110f3d96fec63083f5cb23d4c6c2115ae8f938
Piotr Oniszczuk <warpme@o2.pl>
2017-01-05 18:55:07 +0100

    Update boot.py to disable AP mode on ESP8266. This fixes security hole

--- boot.py
+++ boot.py
@@ -1,19 +1,30 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-
+#import esp
+#esp.osdebug(None)
 import gc
 import webrepl
+import network
+
 webrepl.start()
 gc.collect()
 
 def do_connect():
     import network
+
+    SSID     = '<WiFi networkid'
+    PASSWORD = '<WiFi pass>'
+
     sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        print('-- Disablig AP mode...')
+        ap_if.active(False)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print('-- Connecting to WiFI network...')
         sta_if.active(True)
-        sta_if.connect('<Your WiFi ID', '<Your WiFi pass')
+        sta_if.connect(SSID, PASSWORD)
         while not sta_if.isconnected():
             pass
-    print('network config:', sta_if.ifconfig())
+    print('-- Network configuration:', sta_if.ifconfig())
 
 do_connect()
